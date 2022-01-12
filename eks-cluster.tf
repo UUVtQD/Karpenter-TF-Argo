662fdead158f3e4922bdafd425dc554e0d38a8d8
Sergio <sergii.kotenko@sixt.com>
2022-01-12 13:29:19 +0200

    Added irsa

--- eks-cluster.tf
+++ eks-cluster.tf
@@ -4,6 +4,7 @@ module "eks" {
   cluster_name    = local.cluster_name
   cluster_version = "1.21"
   subnet_ids      = module.vpc.private_subnets
+  enable_irsa     = true
 
   tags = {
     Environment = "karpenter"
